ce4bd6c3a2a3b2171719252f57a1125de7c19235
shpeckman <geelenrobin@proton.me>
2026-08-12 00:26:26 +0200

    chore: remove examples, makefile, and spec files

--- spec/child_probe.sh
+++
@@ -1,7 +0,0 @@
-# spec/child_probe.sh
-sid=$(ps -o sess= -p $$ 2>/dev/null | tr -d ' ')
-[ -z "$sid" ] && sid=$(ps -o sid= -p $$ 2>/dev/null | tr -d ' ')
-echo "SID=${sid} PID=$$"
-if { : < /dev/tty ; } 2>/dev/null; then echo "HAS_CTTY=yes"; else echo "HAS_CTTY=no"; fi
-echo "TTYNAME=$(tty 2>/dev/null)"
-exit 0
